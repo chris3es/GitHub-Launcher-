493cfa4f0ed8d7192df20823e36c9428dcf14c86
chris3es <chrisdebiase2@gmail.com>
2025-11-16 13:45:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.getElementById("launchBtn").addEventListener("click", () => {
   const version = document.getElementById("versionSelect").value;
   const profile = document.getElementById("profileSelect").value;
 
-  // For now, just launch the version and show profile in console
-  console.log("Launching " + version + " with profile " + profile);
+  alert("Launching " + version + " with profile " + profile);
   window.location.href = version;
 });
